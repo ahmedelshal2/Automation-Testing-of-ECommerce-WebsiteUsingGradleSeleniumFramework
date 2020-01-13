d3cac425e771259eca14b85f9f395678acd22090
ahelshal <ahmedmorshedy03@gmail.com>
2020-01-13 21:43:59 +0200

    Gradle Selenium Framework project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Automation-Testing-of-ECommerce-WebsiteUsingSelenium
+- Java
+- Maven
+- TestNG
+- BDD
+- DDT 
+- POM (Page Object Model)
+- Selenium Grid
+- Sauce Labs
+- Zalenium 
+- Docker
+# Integrated Development Environment 
+- Eclipse IDEA
+- IntelliJ IDEA
+
+- It’s demo project https://demo.nopcommerce.com/ , I used Selenium Framework in this project.
+
---
+++ src/test/java/tests/ContactUsTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.ContactUsPage;
+import pages.HomePage;
+
+public class ContactUsTest extends TestBase {
+	
+	HomePage home;
+	ContactUsPage contactpage;
+	String fullNamee="Ahmdhsssedy";
+	String emaill="al4si4dj4d55@gmail.com";
+	String enquiryy="hello admin please help me";
+	@Test
+	@Severity(SeverityLevel.CRITICAL)
+	@Description("Contact US Test Case")
+	@Link(name="JIRA Ticket", url = "https://google.com")
+
+	public void UsercanuseContactUs()
+	{
+		home=new HomePage(driver);
+		home.openContactUsPage();
+		contactpage=new ContactUsPage(driver);
+		contactpage.ContactUs(fullNamee, emaill, enquiryy);
+		Assert.assertTrue(contactpage.successMessage.getText().contains("Your enquiry has been successfully sent to the store owner."));
+	}
+
+}
